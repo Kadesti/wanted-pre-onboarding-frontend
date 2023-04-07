ec13f682dd1d689c617712d035e5f76d4906170e
Kadesti <50021232+Kadesti@users.noreply.github.com>
2023-04-07 10:53:14 +0900

    feat: 라우팅 / Sign, Todo 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { lazy, useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const Todo = lazy(() => import('./Component/Todo'))
+const Sign = lazy(() => import('./Component/Sign'))
+
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/todo" element={<Todo />} />
+          <Route path={'/signin'} element={<Sign />} />
+          <Route path={'/signup'} element={<Sign />} />
+
+          {/* 테스트 코드 */}
+          <Route path="*" element={<Todo />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
