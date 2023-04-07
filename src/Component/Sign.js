656a879f4288c5167cced9a47850688a16a35439
Kadesti <50021232+Kadesti@users.noreply.github.com>
2023-04-07 12:46:08 +0900

    :art: CSS - Styled-component

--- src/Component/Sign.js
+++ src/Component/Sign.js
@@ -1,6 +1,38 @@
 import useInput from './util/useInput';
+import styled from 'styled-components'
+
 import { signClick } from './util/api'
 import Input from './Input';
+
+const SignStyle = styled.form`
+    display: flex;
+    flex-direction: column;
+
+    width: 40%;
+    height: 60%;
+    padding: 3%;
+    border: solid 0.1em black;
+    border-radius: 5%;
+
+    justify-content: center;
+
+    .valueInput {
+       display: flex;
+       justify-content: space-between;
+       margin-bottom: 5%;
+    }
+
+    input {
+        height: 100%;
+        background: none;
+        border-width: 0.1em;
+    }
+
+    input:focus {
+        outline: none;
+    }
+
+`
 
 function Sign({ path }) {
     const emailBind = useInput();
@@ -12,12 +44,12 @@ function Sign({ path }) {
     }
 
     return (
-        <form onSubmit={onSubmit}>
+        <SignStyle onSubmit={onSubmit}>
             <h2>{path}</h2>
             <Input label={"email"} value={emailBind} />
             <Input label={"password"} value={passwordBind} />
             <button data-testid={`${path}-button`} type='submit'>회원가입</button>
-        </form>
+        </SignStyle>
     )
 }
 
--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -1,5 +1,66 @@
 import { useState } from 'react';
 import dummyData from '../data/dummydata'
+import styled from 'styled-components'
+
+const TodoStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    
+    width: 40%;
+    height: 60%;
+    padding: 3%;
+    border: solid 0.1em black;
+    border-radius: 5%;
+
+    /* background-color: #EEE9DA; */
+
+    .new-todo {
+        margin-bottom: 8%;
+
+        input {
+            width: 80%;
+            height: 100%;
+            margin-right: 2%;
+            background: none;
+            border-width: 0.1em;
+        }
+
+        input:focus {
+            outline: none;
+        }
+    }
+
+    li {
+        margin-bottom: 2%;
+    }
+    button {
+        display: inline-block;
+        height: 100%;
+        background: none;
+        position: relative;
+
+        border-color: black;
+        border-radius: 5%;
+        border-width: 0.1em;
+    }
+
+    button:focus {
+        outline: none;
+    }
+
+    label {
+        display: flex;
+        width: 100%;        
+
+        > :nth-child(2) {
+            flex-grow:1;
+        }
+        > :nth-child(3) {
+            margin-right: 0.3em;
+        }
+    }
+
+`
 
 const TodoItem = ({ item, ismodify, setIsModify }) => {
     const [checked, isChecked] = useState(item.isCompleted)
@@ -34,7 +95,7 @@ const Todo = () => {
     const [ismodify, setIsModify] = useState([...dummyData].fill(false));
 
     return (
-        <div>
+        <TodoStyle>
             <div className='new-todo'>
                 <input data-testid="new-todo-input" />
                 <button data-testid="new-todo-add-button">추가</button>
@@ -43,7 +104,7 @@ const Todo = () => {
             <ul>
                 {dummyData.map((item, i) => <TodoItem item={item} ismodify={ismodify[i]} {...setIsModify} key={item.id} />)}
             </ul>
-        </div>
+        </TodoStyle>
     )
 }
 
