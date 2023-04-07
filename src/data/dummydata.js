1a55fd4df58ffd6f585f79324ae1cd33d7060401
Kadesti <50021232+Kadesti@users.noreply.github.com>
2023-04-07 12:11:58 +0900

    :memo: Feat : Todo DOM

---
+++ src/data/dummydata.js
@@ -0,0 +1,17 @@
+const dummyData =
+    [
+        {
+            "id": 1,
+            "todo": "todo2",
+            "isCompleted": false,
+            "userId": 1
+        },
+        {
+            "id": 2,
+            "todo": "todo3",
+            "isCompleted": false,
+            "userId": 1
+        }
+    ]
+
+export default dummyData;
