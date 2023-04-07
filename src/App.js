812e2137399cd1af859393e91da5befb00730d96
Kadesti <50021232+Kadesti@users.noreply.github.com>
2023-04-07 11:10:26 +0900

    feat : Sign DOM 구현
    Signup 과 Signin을 동일한 UI로 구현해도 무방하다

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ const Sign = lazy(() => import('./Component/Sign'))
 function App() {
   const [isLogin, setIsLogin] = useState(false);
 
+  const signin = 'signin';
+  const signup = 'signup';
+
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/todo" element={<Todo />} />
-          <Route path={'/signin'} element={<Sign />} />
-          <Route path={'/signup'} element={<Sign />} />
+          <Route path={`/${signin}`} element={<Sign path={signin} />} />
+          <Route path={`/${signup}`} element={<Sign path={signup} />} />
 
           {/* 테스트 코드 */}
           <Route path="*" element={<Todo />} />
--- src/Component/Sign.js
+++ src/Component/Sign.js
@@ -1,8 +1,22 @@
+import { useState } from 'react'
 
+function Sign({ path }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-function Sign() {
     return (
-        <div>Sign</div>
+        <div>
+            <h2>{path}</h2>
+            <div>
+                <label>email</label>
+                <input data-testid="email-input" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+            </div>
+            <div>
+                <label>password</label>
+                <input data-testid="password-input" value={password} onChange={(e) => { setPassword(e.target.value) }} />
+            </div>
+            <button data-testid={`${path}-button`} type='submit'>회원가입</button>
+        </div>
     )
 }
 
